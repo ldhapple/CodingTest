abaa78bb2540586d7a172afbee72c5c1ef9ad804
leedohyun <lucky77795@naver.com>
2023-07-19 15:48:33 +0900

    벨만포드, 플로이드 워셜

--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,7 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    static int N;
-    static int M;
-    static long[] time;
-    static final long INF = Integer.MAX_VALUE;
-
-    static Bus busArr[];
-    public static class Bus{
-        public int start;
-        public int end;
-        public int weight;
-
-        public Bus(int start, int end, int weight){
-            this.start = start;
-            this.end = end;
-            this.weight = weight;
-        }
-    }
-
+    static final int INF = 987654321;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
@@ -30,61 +13,48 @@ public class Main {
 
         st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken()); // 도시의 개수
-        M = Integer.parseInt(st.nextToken()); // 버스 노선의 개수
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
 
-        busArr = new Bus[M];
-        time = new long[N+1];
+        int[][] arr = new int[V+1][V+1];
 
-        Arrays.fill(time, INF);
+        for(int i = 1; i <= V; i++){
+            for(int j = 1; j <= V; j++){
+                if(i == j) arr[i][j] = 0;
+                else arr[i][j] = INF;
+            }
+        }
 
-        for(int i = 0; i < M; i++){
+        for(int i = 0; i < E; i++){
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
 
-            busArr[i] = new Bus(start, end, weight);
+            arr[u][v] = d;
         }
 
-        if(bellmanFord()){
-            for(int i = 2; i <= N; i++){
-                if(time[i] == INF){
-                    sb.append(-1).append("\n");
-                }
-                else{
-                    sb.append(time[i]).append("\n");
+        for(int k = 1; k <= V; k++){
+            for(int i = 1; i <= V; i++){
+                for(int j = 1; j <= V; j++){
+                    if(arr[i][j] > arr[i][k] + arr[k][j]){
+                        arr[i][j] = arr[i][k] + arr[k][j];
+                    }
                 }
             }
-        }else{
-            sb.append(-1);
         }
 
-        System.out.print(sb);
+        int min = Integer.MAX_VALUE;
+        for(int i = 1; i <= V; i++){
+            for(int j = 1; j <= V; j++){
+                if(i == j) continue;
+
+                if(arr[i][j] != INF && arr[j][i] != INF){
+                    min = Math.min(min, arr[i][j] + arr[j][i]);
+                }
+            }
+        }
+        if(min == Integer.MAX_VALUE) min = -1;
+        System.out.print(min);
     }
-
-    private static boolean bellmanFord(){
-        time[1] = 0; //시작점
-
-        for(int i = 1; i < N; i++){
-            for(int j = 0; j < M; j++){
-                Bus bus = busArr[j];
-
-                if(time[bus.start] != INF && time[bus.end] > time[bus.start] + bus.weight){
-                    time[bus.end] = time[bus.start] + bus.weight;
-                }
-            }
-        }
-
-        //음수 사이클 확인
-        for(int i = 0; i < M; i++){
-            Bus bus = busArr[i];
-
-            if(time[bus.start] != INF && time[bus.end] > time[bus.start] + bus.weight){
-                return false;
-            }
-        }
-        return true;
-    }
-
 }
