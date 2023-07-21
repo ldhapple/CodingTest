83ef1a6521b891472c8edeca2ddb97f89f2111b7
leedohyun <lucky77795@naver.com>
2023-07-21 13:15:29 +0900

    스도쿠

--- src/Main.java
+++ src/Main.java
@@ -7,44 +7,83 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    static Node root = new Node();
-    static class Node{
-        HashMap<String, Node> child;
-        public Node(){
-            child = new HashMap<>();
-        }
-    }
     static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    static int[][] table;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
-        int N = Integer.parseInt(br.readLine());
+        table = new int[9][9];
 
-        for(int i=0;i<N;i++){
+        for(int i = 0; i < 9; i++){
             st = new StringTokenizer(br.readLine());
-            int K = Integer.parseInt(st.nextToken());
-            Node cur = root;
-            for(int j=0;j<K;j++){
-                String temp = st.nextToken();
-                if(!cur.child.containsKey(temp)){
-                    cur.child.put(temp, new Node());
-                }
-                cur = cur.child.get(temp);
+            for(int j = 0; j < 9; j++){
+                int next = Integer.parseInt(st.nextToken());
+                table[i][j] = next;
             }
         }
-        print(root, "");
-        System.out.print(sb);
+
+        sudoku(0, 0);
+
     }
 
-    static void print(Node cur, String s){
-        //현재 방에 자식 구조 가져오기
-        ArrayList<String> list = new ArrayList<>(cur.child.keySet());
-        Collections.sort(list);		//사전 순 정렬
-        //정렬 이후 사전 순으로 표시 후 탐색
-        for(String str : list){
-            sb.append(s).append(str).append("\n");
-            print(cur.child.get(str), s +"--");
+    public static void sudoku(int row, int col){
+        if(col == 9){
+            sudoku(row + 1, 0);
+            return;
         }
+
+        if(row == 9){
+            for(int i = 0; i < 9; i++){
+                for(int j = 0; j < 9; j++){
+                    sb.append(table[i][j]).append(" ");
+                }
+                sb.append("\n");
+            }
+            System.out.print(sb);
+            System.exit(0);
+        }
+
+        if(table[row][col] == 0){
+            for(int i = 1; i <= 9; i++){
+                if(possibility(row, col, i)){
+                    table[row][col] = i;
+                    sudoku(row, col + 1);
+                }
+            }
+            table[row][col] = 0;
+            return;
+        }
+
+        sudoku(row, col + 1);
     }
+
+    public static boolean possibility(int row, int col, int value){
+        for(int i = 0; i < 9; i++){
+            if(table[row][i] == value){
+                return false;
+            }
+        }
+
+        for(int i = 0; i < 9; i++){
+            if(table[i][col] == value){
+                return false;
+            }
+        }
+
+        int ract_row = (row/3) * 3;
+        int ract_col = (col/3) * 3;
+
+        for(int i = ract_row; i < ract_row + 3; i++){
+            for(int j = ract_col; j < ract_col + 3; j++){
+                if(table[i][j] == value){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
 }
