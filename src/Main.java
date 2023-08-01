e7488433e996d46e6b43cd2cdc97a5ae017ed71d
leedohyun <lucky77795@naver.com>
2023-08-01 23:13:23 +0900

    자료구조(스택, 큐, 덱)

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import java.awt.*;
+import java.awt.image.AreaAveragingScaleFilter;
 import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
 import java.math.BigInteger;
 import java.security.cert.CollectionCertStoreParameters;
+import java.sql.Array;
 import java.util.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -11,70 +13,43 @@ public class Main {
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
 
-    static int[] dx = {-1,-1,0,1,1,1,0,-1};
-    static int[] dy = {0,1,1,1,0,-1,-1,-1};
-    static int N, M, K, MAX_LENGTH;
-    static Map<String, Integer> map = new HashMap<>();
-    static char[][] arr;
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        st = new StringTokenizer(br.readLine());
+        String str = br.readLine();
+        String check = br.readLine();
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        Stack<Character> stack = new Stack<>();
 
-        K = Integer.parseInt(st.nextToken());
+        for(int i = 0; i < str.length(); i++){
+            stack.push(str.charAt(i));
 
-        arr = new char[N][M];
-        MAX_LENGTH = 5;
+            if(stack.size() >= check.length()){
+                boolean flag = true;
 
-        for(int i = 0; i < N; i++){
-            arr[i] = br.readLine().toCharArray();
-        }
+                for(int j = 0; j < check.length(); j++){
+                    if(stack.get(stack.size() - check.length() + j) != check.charAt(j)){
+                        flag = false;
+                        break;
+                    }
+                }
 
-        String[] string_arr = new String[K];
-
-        for(int i = 0; i < K; i++){
-            String favoriteString = br.readLine();
-            //MAX_LENGTH = Math.max(MAX_LENGTH, favoriteString.length());
-
-            map.put(favoriteString, 0);
-            string_arr[i] = favoriteString;
-        }
-
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < M; j++){
-                dfs(i,j,1, Character.toString(arr[i][j]));
+                if(flag){
+                    for(int j = 0; j < check.length(); j++){
+                        stack.pop();
+                    }
+                }
             }
         }
 
-        for(String key : string_arr){
-            sb.append(map.get(key)).append("\n");
+        for(Character c : stack){
+            sb.append(c);
         }
 
+        if(sb.toString().equals("")){
+            sb.append("FRULA");
+        }
         System.out.print(sb);
     }
 
-    public static void dfs(int x, int y, int depth, String result){
-        if(map.containsKey(result)){
-            map.put(result, map.get(result) + 1);
-        }
-
-        if(depth == MAX_LENGTH) return;
-
-        for(int i = 0; i < dx.length; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if(nx < 0) nx = N-1;
-            else if(nx >= N) nx = 0;
-
-            if(ny < 0) ny = M-1;
-            else if(ny >= M) ny = 0;
-
-            dfs(nx,ny,depth+1,result + arr[nx][ny]);
-        }
-    }
 }
