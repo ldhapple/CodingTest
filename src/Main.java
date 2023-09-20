428feb3954f7cd6f44956b362b9336101ddf1752
leedohyun <lucky77795@naver.com>
2023-09-20 09:39:35 +0900

    9/20

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
-import java.awt.*;
-import java.awt.image.AreaAveragingScaleFilter;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
+import javax.print.attribute.IntegerSyntax;
 import java.math.BigInteger;
-import java.security.cert.CollectionCertStoreParameters;
-import java.sql.Array;
 import java.util.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -12,44 +7,9 @@ import java.io.InputStreamReader;
 public class Main {
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String str = br.readLine();
-        String check = br.readLine();
 
-        Stack<Character> stack = new Stack<>();
-
-        for(int i = 0; i < str.length(); i++){
-            stack.push(str.charAt(i));
-
-            if(stack.size() >= check.length()){
-                boolean flag = true;
-
-                for(int j = 0; j < check.length(); j++){
-                    if(stack.get(stack.size() - check.length() + j) != check.charAt(j)){
-                        flag = false;
-                        break;
-                    }
-                }
-
-                if(flag){
-                    for(int j = 0; j < check.length(); j++){
-                        stack.pop();
-                    }
-                }
-            }
-        }
-
-        for(Character c : stack){
-            sb.append(c);
-        }
-
-        if(sb.toString().equals("")){
-            sb.append("FRULA");
-        }
-        System.out.print(sb);
     }
-
 }
