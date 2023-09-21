c9015a11250d3a531fd999d4e668190ac09983f6
leedohyun <lucky77795@naver.com>
2023-09-21 12:38:48 +0900

    9/20

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,28 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int N = Integer.parseInt(br.readLine());
 
+        HashSet<Long> sosu = new HashSet<>();
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < N; i++){
+            BigInteger bi = new BigInteger(st.nextToken());
+
+            if(bi.isProbablePrime(10)){
+                sosu.add(Long.parseLong(bi.toString()));
+            }
+        }
+
+        if(sosu.isEmpty()){
+            System.out.print(-1);
+        }else{
+            long answer = 1;
+            for(Long num : sosu){
+                answer *= num;
+            }
+            System.out.print(answer);
+        }
     }
 }
