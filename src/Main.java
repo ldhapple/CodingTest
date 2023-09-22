e2725170db0e64c3ecc6dbbd87591c376143aa8d
leedohyun <lucky77795@naver.com>
2023-09-22 11:14:40 +0900

    9/20

--- src/Main.java
+++ src/Main.java
@@ -7,31 +7,10 @@ import java.io.InputStreamReader;
 public class Main {
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
 
-        HashSet<Long> sosu = new HashSet<>();
-
-        st = new StringTokenizer(br.readLine());
-
-        for(int i = 0; i < N; i++){
-            BigInteger bi = new BigInteger(st.nextToken());
-
-            if(bi.isProbablePrime(10)){
-                sosu.add(Long.parseLong(bi.toString()));
-            }
-        }
-
-        if(sosu.isEmpty()){
-            System.out.print(-1);
-        }else{
-            long answer = 1;
-            for(Long num : sosu){
-                answer *= num;
-            }
-            System.out.print(answer);
-        }
     }
 }
