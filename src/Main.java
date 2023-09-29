49cdaa101d0f32037e624cce3bde9c1a7b17214f
leedohyun <lucky77795@naver.com>
2023-09-29 21:42:59 +0900

    feat: 백준 빙고 풀이
    구현 부분 약해 쉬운 것 부터
    
    resolves: #2578

--- src/Main.java
+++ src/Main.java
@@ -12,64 +12,94 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        st = new StringTokenizer(br.readLine());
+        List<ArrayList<Integer>> table = new ArrayList<>();
+        boolean[][] isVisited = new boolean[5][5];
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-
-        int[] arr = new int[N];
-
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < 5; i++) {
+            table.add(new ArrayList<>());
         }
 
-        for (int i = 0; i < M; i++) {
+        for (int i = 0; i < 5; i++) {
             st = new StringTokenizer(br.readLine());
-
-            int order = Integer.parseInt(st.nextToken());
-            int start = 0;
-            int end = 0;
-
-            switch (order) {
-                case 1:
-                    int num = Integer.parseInt(st.nextToken()) - 1;
-                    arr[num] = Integer.parseInt(st.nextToken());
-                    break;
-
-                case 2:
-                    start = Integer.parseInt(st.nextToken()) - 1;
-                    end = Integer.parseInt(st.nextToken()) - 1;
-                    for (int k = start; k <= end; k++) {
-                        if (arr[k] == 0) arr[k] = 1;
-                        else arr[k] = 0;
-                    }
-                    break;
-
-                case 3:
-                    start = Integer.parseInt(st.nextToken()) - 1;
-                    end = Integer.parseInt(st.nextToken()) - 1;
-                    for (int k = start; k <= end; k++) {
-                        if (arr[k] == 0) continue;
-                        arr[k] = 0;
-                    }
-                    break;
-
-                case 4:
-                    start = Integer.parseInt(st.nextToken()) - 1;
-                    end = Integer.parseInt(st.nextToken()) - 1;
-                    for (int k = start; k <= end; k++) {
-                        if (arr[k] == 1) continue;
-                        arr[k] = 1;
-                    }
-                    break;
+            for (int j = 0; j < 5; j++) {
+                table.get(i).add(Integer.parseInt(st.nextToken()));
             }
         }
 
-        for (int i : arr) {
-            sb.append(i + " ");
-        }
+        int cnt = 0;
+        int bingo_cnt = 0;
+        for (int i = 0;  i < 5; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 5; j++) {
+                int check_num = Integer.parseInt(st.nextToken());
+                cnt++;
+                int y = 0;
+                int x = 0;
 
-        System.out.print(sb);
+                for (int k = 0; k < 5; k++) {
+                    int check = table.get(k).indexOf(check_num);
+                    if (check != -1) {
+                        y = k;
+                        x = check;
+                    }
+                }
+
+                isVisited[y][x] = true;
+
+                if (check_row(y, isVisited)) bingo_cnt++;
+                if (check_col(x, isVisited)) bingo_cnt++;
+                if (x + y == 4) {
+                    if (check_left(y, x, isVisited)) bingo_cnt++;
+                }
+
+                if (x == y) {
+                    if (check_right(y, x, isVisited)) bingo_cnt++;
+                }
+
+
+                if(bingo_cnt >= 3) {
+                    System.out.print(cnt);
+                    System.exit(0);
+                }
+            }
+        }
+    }
+
+    public static boolean check_row(int y, boolean[][] isVisited) {
+        for (int i = 0; i < 5; i++) {
+            if (isVisited[y][i]) continue;
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean check_col(int x, boolean[][] isVisited) {
+        for (int i = 0; i < 5; i++) {
+            if (isVisited[i][x]) continue;
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean check_left(int y, int x, boolean[][] isVisited) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 4; j >= 0; j--) {
+                if (i + j != 4) continue;
+                if (isVisited[i][j]) continue;
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean check_right(int y, int x, boolean[][] isVisited) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (i != j) continue;
+                if (isVisited[i][j]) continue;
+                return false;
+            }
+        }
+        return true;
     }
 }
