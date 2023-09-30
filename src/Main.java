0f893d74b7aefe06def9c0b0293c7a1cdcab1341
leedohyun <lucky77795@naver.com>
2023-09-30 13:55:11 +0900

    feat: 백준 스티커 풀이
    dp, 구현 연습 많이 필요
    
    resolves: #9465

--- src/Main.java
+++ src/Main.java
@@ -12,94 +12,29 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        List<ArrayList<Integer>> table = new ArrayList<>();
-        boolean[][] isVisited = new boolean[5][5];
+        int T = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < 5; i++) {
-            table.add(new ArrayList<>());
-        }
+        for (int i = 0; i < T; i++) {
+            int n = Integer.parseInt(br.readLine());
+            int[][] table = new int[2][n+1];
+            int[][] dp = new int[2][n+1];
 
-        for (int i = 0; i < 5; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 5; j++) {
-                table.get(i).add(Integer.parseInt(st.nextToken()));
-            }
-        }
-
-        int cnt = 0;
-        int bingo_cnt = 0;
-        for (int i = 0;  i < 5; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 5; j++) {
-                int check_num = Integer.parseInt(st.nextToken());
-                cnt++;
-                int y = 0;
-                int x = 0;
-
-                for (int k = 0; k < 5; k++) {
-                    int check = table.get(k).indexOf(check_num);
-                    if (check != -1) {
-                        y = k;
-                        x = check;
-                    }
-                }
-
-                isVisited[y][x] = true;
-
-                if (check_row(y, isVisited)) bingo_cnt++;
-                if (check_col(x, isVisited)) bingo_cnt++;
-                if (x + y == 4) {
-                    if (check_left(y, x, isVisited)) bingo_cnt++;
-                }
-
-                if (x == y) {
-                    if (check_right(y, x, isVisited)) bingo_cnt++;
-                }
-
-
-                if(bingo_cnt >= 3) {
-                    System.out.print(cnt);
-                    System.exit(0);
+            for (int j = 0; j < 2; j++) {
+                st = new StringTokenizer(br.readLine());
+                for (int k = 1; k <= n; k++) {
+                    table[j][k] = Integer.parseInt(st.nextToken());
                 }
             }
-        }
-    }
 
-    public static boolean check_row(int y, boolean[][] isVisited) {
-        for (int i = 0; i < 5; i++) {
-            if (isVisited[y][i]) continue;
-            return false;
-        }
-        return true;
-    }
+            dp[0][1] = table[0][1];
+            dp[1][1] = table[1][1];
 
-    public static boolean check_col(int x, boolean[][] isVisited) {
-        for (int i = 0; i < 5; i++) {
-            if (isVisited[i][x]) continue;
-            return false;
-        }
-        return true;
-    }
-
-    public static boolean check_left(int y, int x, boolean[][] isVisited) {
-        for (int i = 0; i < 5; i++) {
-            for (int j = 4; j >= 0; j--) {
-                if (i + j != 4) continue;
-                if (isVisited[i][j]) continue;
-                return false;
+            for (int j = 2; j <= n; j++) {
+                dp[0][j] = Math.max(dp[1][j - 1], dp[1][j-2]) + table[0][j];
+                dp[1][j] = Math.max(dp[0][j - 1], dp[0][j-2]) + table[1][j];
             }
-        }
-        return true;
-    }
 
-    public static boolean check_right(int y, int x, boolean[][] isVisited) {
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                if (i != j) continue;
-                if (isVisited[i][j]) continue;
-                return false;
-            }
+            System.out.println(Math.max(dp[0][n], dp[1][n]));
         }
-        return true;
     }
 }
