6e9ee38cd0e7f4088275508440445f8ba910dd22
leedohyun <lucky77795@naver.com>
2023-10-01 22:33:28 +0900

    chore: Optional, Stream 연습
    Stream 연습
    
    resolves:

--- src/Main.java
+++ src/Main.java
@@ -3,38 +3,29 @@ import java.math.BigInteger;
 import java.util.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 
 public class Main {
 
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
 
+    public static class Student {
+        public String name;
+        public int num;
+
+        public Student(String name, int num) {
+            this.name = name;
+            this.num = num;
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int T = Integer.parseInt(br.readLine());
+        List<Student> students = Arrays.asList(new Student("lee", 10),
+                new Student("hong", 14), new Student("kim", 16));
 
-        for (int i = 0; i < T; i++) {
-            int n = Integer.parseInt(br.readLine());
-            int[][] table = new int[2][n+1];
-            int[][] dp = new int[2][n+1];
-
-            for (int j = 0; j < 2; j++) {
-                st = new StringTokenizer(br.readLine());
-                for (int k = 1; k <= n; k++) {
-                    table[j][k] = Integer.parseInt(st.nextToken());
-                }
-            }
-
-            dp[0][1] = table[0][1];
-            dp[1][1] = table[1][1];
-
-            for (int j = 2; j <= n; j++) {
-                dp[0][j] = Math.max(dp[1][j - 1], dp[1][j-2]) + table[0][j];
-                dp[1][j] = Math.max(dp[0][j - 1], dp[0][j-2]) + table[1][j];
-            }
-
-            System.out.println(Math.max(dp[0][n], dp[1][n]));
-        }
+        Optional<Student> student1 = students.stream().filter(s -> s.num == 10).findFirst().orElse(null);
     }
 }
