ea95105a1334306b41ba319123d29f9d9ec9bf78
leedohyun <lucky77795@naver.com>
2023-10-02 12:25:37 +0900

    feat: 돌 게임
    resolves: #9655

--- src/Main.java
+++ src/Main.java
@@ -10,22 +10,11 @@ public class Main {
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
 
-    public static class Student {
-        public String name;
-        public int num;
-
-        public Student(String name, int num) {
-            this.name = name;
-            this.num = num;
-        }
-    }
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        List<Student> students = Arrays.asList(new Student("lee", 10),
-                new Student("hong", 14), new Student("kim", 16));
+        int N = Integer.parseInt(br.readLine());
 
-        Optional<Student> student1 = students.stream().filter(s -> s.num == 10).findFirst().orElse(null);
+        System.out.println(N%2 == 0 ? "CY" : "SK");
     }
 }
