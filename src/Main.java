f69428626226ec4e1c246da66e345c3057bc7914
leedohyun <lucky77795@naver.com>
2023-10-11 16:52:05 +0900

    feat: 평범한 배낭
    dp로 값을 구할 때, 순회했던 부분은 이미 최대나 최소의 조합이므로
    이후에 어떤 것이 추가되어도 그 조합이 최선임을 기억하자.
    
    resolves: #12865

--- src/Main.java
+++ src/Main.java
@@ -10,49 +10,52 @@ public class Main {
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
 
+    static class State {
+        int weight;
+        int value;
+
+        public State(int weight, int value) {
+            this.weight = weight;
+            this.value = value;
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken()); //돌의 개수
-        int K = Integer.parseInt(st.nextToken()); //최대 힘
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[N];
+        State[] arr = new State[N+1];
+        arr[0] = new State(0,0);
 
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int w = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            arr[i] = new State(w, v);
         }
 
-        int[] dp = new int[N];
-        Arrays.fill(dp, 1001);
+        int[][] dp = new int[N+1][K+1];
 
-        dp[0] = 0;
+        for (int i = 1; i <= N; i++) {
+            /**
+             * i번 물건까지의 무게별 최대 가치의 합을 찾았다면
+             * 이후 어떤 물건이 와도, 이전 물건의 조합에 변경이 있을 수 없다.
+             * 따라서 i=물건별 순회
+             * j = 무게별 순회
+             */
 
-        for (int i = 1; i < N; i++) {
-            boolean flag = false;
-            for (int j = 0; j < i; j++) {
-                int power = power(j, i, arr);
-
-                if (power <= K && dp[j] != -1) {
-                    flag = true;
-                    dp[i] = Math.min(dp[i], dp[j] + power);
+            for (int j = 1; j <= K; j++) {
+                if (arr[i].weight > j) {
+                    dp[i][j] = dp[i-1][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-arr[i].weight] + arr[i].value);
                 }
             }
-            if (!flag) {
-                dp[i] = -1;
-            }
         }
-
-        if (dp[N-1] != -1) {
-            System.out.print("YES");
-        } else {
-            System.out.print("NO");
-        }
-    }
-
-    public static int power(int start, int fin, int[] arr) {
-        return (fin - start) * (1 + Math.abs(arr[start] - arr[fin]));
+        System.out.print(dp[N][K]);
     }
 }
