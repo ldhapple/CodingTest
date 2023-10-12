021527df2ada992a619aaaee668326b5fd9077a7
leedohyun <lucky77795@naver.com>
2023-10-12 23:22:32 +0900

    feat: 합 분해
    2차원 dp배열 활용 연습 필요
    
    resolves: #2225

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,13 @@
-import javax.print.attribute.IntegerSyntax;
-import java.math.BigInteger;
-import java.util.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.stream.Collectors;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
-
-    static class State {
-        int weight;
-        int value;
-
-        public State(int weight, int value) {
-            this.weight = weight;
-            this.value = value;
-        }
-    }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,34 +17,28 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        State[] arr = new State[N+1];
-        arr[0] = new State(0,0);
 
-        for (int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine());
-            int w = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            arr[i] = new State(w, v);
+        /**
+         * dp[1] = 1이 K개의 숫자 합으로 만들어지는 개수
+         * 숫자의 중복이 가능하다.
+         * N이 1이고, K가 2라면
+         * 0부터이기 떄문에 (0,1), (1,0) 2가지의 경우가 있다.
+         */
+
+        int[][] dp = new int[K + 1][N + 1];
+        Arrays.fill(dp[1], 1);
+
+        for (int i = 1; i <= K; i++) {
+            dp[i][0] = 1;
         }
 
-        int[][] dp = new int[N+1][K+1];
-
-        for (int i = 1; i <= N; i++) {
-            /**
-             * i번 물건까지의 무게별 최대 가치의 합을 찾았다면
-             * 이후 어떤 물건이 와도, 이전 물건의 조합에 변경이 있을 수 없다.
-             * 따라서 i=물건별 순회
-             * j = 무게별 순회
-             */
-
-            for (int j = 1; j <= K; j++) {
-                if (arr[i].weight > j) {
-                    dp[i][j] = dp[i-1][j];
-                } else {
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-arr[i].weight] + arr[i].value);
-                }
+        for (int i = 2; i <= K; i++) {
+            for (int j = 1; j <= N; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                dp[i][j] %= 1000000000;
             }
         }
-        System.out.print(dp[N][K]);
+
+        System.out.print(dp[K][N]);
     }
 }
