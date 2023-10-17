56707d28ec3eb15b063c1d22da300a200311ab81
leedohyun <lucky77795@naver.com>
2023-10-17 11:27:45 +0900

    feat: 벼락치기
    2차원 dp 성공.
    
    resolves: #14728

--- src/Main.java
+++ src/Main.java
@@ -9,36 +9,47 @@ public class Main {
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
 
+    static class Subject {
+        int hour;
+        int score;
+
+        public Subject(int hour, int score) {
+            this.hour = hour;
+            this.score = score;
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int T = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
 
-        for (int t = 0; t < T; t++) {
-            int N = Integer.parseInt(br.readLine()); //동전의 가지 수
+        int N = Integer.parseInt(st.nextToken()); //단원의 수
+        int T = Integer.parseInt(st.nextToken()); //공부할 수 있는 시간
 
-            int[] coins = new int[N + 1];
-            long[][] dp = new long[N+1][10001];
+        Subject[] subjects = new Subject[N+1];
 
+        for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int i = 1; i <= N; i++) {
-                coins[i] = Integer.parseInt(st.nextToken());
-                dp[i][coins[i]] += 1;
-            }
 
-            int target = Integer.parseInt(br.readLine()); //목표 금액
+            int h = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
 
-            for (int i = 1; i <= N; i++) {
-                for (int j = 1; j <= target; j++) {
-                    if (j - coins[i] < 0) {
-                        dp[i][j] = dp[i-1][j];
-                        continue;
-                    }
-                    dp[i][j] += dp[i-1][j] + dp[i][j - coins[i]];
-                }
-            }
-
-            System.out.println(dp[N][target]);
+            subjects[i] = new Subject(h,s);
         }
+
+        int[][] dp = new int[N+1][T+1]; //정답은 dp[N][T]
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= T; j++) {
+                if (j - subjects[i].hour < 0) {
+                    dp[i][j] = dp[i-1][j];
+                    continue;
+                }
+                dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j - subjects[i].hour] + subjects[i].score);
+            }
+        }
+
+        System.out.print(dp[N][T]);
     }
 }
