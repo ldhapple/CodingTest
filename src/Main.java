996a84df6aa76b47f838859688bc129d04af6091
leedohyun <lucky77795@naver.com>
2023-10-21 21:27:42 +0900

    feat: 친구비
    유니온파인드 실패. 낮은 난이도 조금 더 해본 다음 시도해봐야 할 듯.
    
    resolves: #16562

--- src/Main.java
+++ src/Main.java
@@ -9,69 +9,64 @@ public class Main {
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
 
-    static int R;
-    static int C;
-    static int N;
-    static char[][] table;
-    static int[][] isBoom;
+    static class Friend {
+        Friend parent = null;
 
-    static int[] dy = {1, -1, 0, 0};
-    static int[] dx = {0, 0, 1, -1};
+        public Friend root() {
+            if (parent == null) return this;
+            return parent = parent.root();
+        }
+
+        public boolean isConnected(Friend o) {
+            return root() == o.root();
+        }
+
+        public void merge(Friend o) {
+            if (isConnected(o)) return;
+            o.root().parent = this;
+        }
+    }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         st = new StringTokenizer(br.readLine());
-        R = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken()); //N초가 지난 후의 격자판 상태
+        int N = Integer.parseInt(st.nextToken()); //학생 수
+        int M = Integer.parseInt(st.nextToken()); //친구 관계 수
+        int k = Integer.parseInt(st.nextToken()); //가지고 있는 금액
 
-        table = new char[R][C];
-        isBoom = new int[R][C];
+        st = new StringTokenizer(br.readLine());
+        int[] friendCost = new int[N + 1];
+        List<Friend> friendList = new ArrayList<>();
+        friendList.add(new Friend());
+        for (int i = 1; i <= N; i++) {
+            friendCost[i] = Integer.parseInt(st.nextToken());
+            friendList.add(new Friend());
+        }
 
-        for (int i = 0; i < R; i++) {
-            String str = br.readLine();
-            for (int j = 0; j < C; j++) {
-                table[i][j] = str.charAt(j);
-                if (str.charAt(j) == 'O') {
-                    isBoom[i][j] = 3;
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            Friend friendA = friendList.get(Integer.parseInt(st.nextToken()));
+            Friend friendB = friendList.get(Integer.parseInt(st.nextToken()));
+            if (friendA.isConnected(friendB)) continue;
+            friendA.merge(friendB);
+        }
+
+        Map<Friend, Integer> union = new HashMap<>();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (i == j) continue;
+
+                Friend friendA = friendList.get(i);
+                Friend friendB = friendList.get(j);
+
+                if (friendA.isConnected(friendB)) {
+                    union.put(friendA.root(), union.getOrDefault(friendA.root(), 0) + 1);
                 }
             }
         }
 
-        int sec = 0;
-        while(sec++ < N) {
-            if (sec == 1) continue;
-
-            if (sec % 2 == 0) {
-                for (int i = 0; i < R; i++) {
-                    for (int j = 0; j < C; j++) {
-                        if (table[i][j] == '.') {
-                            isBoom[i][j] = sec + 3;
-                            table[i][j] = 'O';
-                        }
-                    }
-                }
-            }else if (sec % 2 != 0) {
-                for (int i = 0; i < R; i++) {
-                    for (int j = 0; j < C; j++) {
-                        if (isBoom[i][j] != sec) continue;
-                        table[i][j] = '.';
-                        for (int k = 0; k < 4; k++) {
-                            int ny = i + dy[k];
-                            int nx = j + dx[k];
-
-                            if (ny < 0 || nx < 0 || ny >= R || nx >= C) continue;
-
-                            table[ny][nx] = '.';
-                        }
-                    }
-                }
-            }
-        }
-
-        for (int i = 0; i < R; i++) {
-            System.out.println(table[i]);
-        }
+        System.out.print(union);
     }
 }
