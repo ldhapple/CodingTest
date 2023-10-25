f9fc24de26a8e6cda3728c75c299f54d99e4ac20
leedohyun <lucky77795@naver.com>
2023-10-26 07:29:28 +0900

    feat: 염색체
    정규표현식 사용에 익숙해지기
    
    resolves: #9342

--- src/Main.java
+++ src/Main.java
@@ -9,51 +9,18 @@ public class Main {
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
 
-    private static class Node{
-        private Node parent = null;
-
-        public boolean isConnected(Node o){
-            return root() == o.root();
-        }
-
-        public void merge(Node o){
-            if(isConnected(o)) return;
-            o.root().parent = this;
-        }
-
-        private Node root(){
-            if(parent == null) return this; // 루트노드 전용
-            return parent = parent.root();
-        }
-    }
-
+    static final String REGEX = "^[A-F]?A+F+C+[A-F]?$";
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        Node[] nodes = new Node[N+1];
 
-        for (int i = 1; i <= N; i++) {
-            nodes[i] = new Node();
-        }
-
-        for (int i = 0; i < N-2; i++) {
-            st = new StringTokenizer(br.readLine());
-            int num1 = Integer.parseInt(st.nextToken());
-            int num2 = Integer.parseInt(st.nextToken());
-            Node node1 = nodes[num1];
-            Node node2 = nodes[num2];
-
-
-            if (node1.isConnected(node2)) continue;
-            node1.merge(node2);
-        }
-
-
-        for (int i = 2; i <= N; i++) {
-            if (!nodes[1].isConnected(nodes[i])) {
-                System.out.print(1 + " " + i);
-                break;
+        for (int t = 0; t < N; t++) {
+            String str = br.readLine();
+            if (str.matches(REGEX)) {
+                System.out.println("Infected!");
+            } else {
+                System.out.println("Good");
             }
         }
     }
