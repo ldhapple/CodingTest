4a17ae2e2d524aa08d2ebe0ea5f4eec63a1ac76d
leedohyun <lucky77795@naver.com>
2023-10-29 23:06:49 +0900

    feat: 문서 검색
    Matcher.find()는 matches()와 다르게 일부가 맞으면 true를 반환하고, 한 번 true를 반환하면 찾은 위치 다음 인덱스부터 다시 찾는다.
    
    resolves: #1543

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 import java.util.*;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
@@ -13,21 +14,20 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        String pt = br.readLine();
-        int starIndex = pt.indexOf("*");
-        String start = pt.substring(0, starIndex);
-        String end = pt.substring(starIndex+1, pt.length());
-        Pattern pattern = Pattern.compile("^(" + start + ")+[a-z]*(" + end + ")+$");
+        String str = br.readLine();
+        String regexStr = br.readLine();
 
-        for (int i = 0; i < N; i++) {
-            String str = br.readLine();
+        Pattern pattern = Pattern.compile(regexStr);
+        Matcher matcher = pattern.matcher(str);
 
-            if (pattern.matcher(str).matches()) {
-                System.out.println("DA");
-            } else {
-                System.out.println("NE");
-            }
+        int cnt = 0;
+//        System.out.print(matcher.find());
+//        System.out.print(matcher.find());
+//        System.out.print(matcher.find());
+        while(matcher.find()) {
+            cnt++;
         }
+
+        System.out.print(cnt);
     }
 }
