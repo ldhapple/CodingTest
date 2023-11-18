5660912fd9f483dc78ac6a9b898f13fa30113695
leedohyun <lucky77795@naver.com>
2023-11-19 04:01:17 +0900

    feat: 스티커
    dp 복습
    
    resolves: #9465

--- src/Main.java
+++ src/Main.java
@@ -9,78 +9,35 @@ public class Main {
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
 
-    public static class Point {
-        public int y;
-        public int x;
-
-        public Point(int y, int x) {
-            this.y = y;
-            this.x = x;
-        }
-
-        public void setY(int y) {
-            this.y = y;
-        }
-
-        public void setX(int x) {
-            this.x = x;
-        }
-    }
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-//        int T = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine());
 
-        for (int test = 1; test <= 10; test++) {
-            int T = Integer.parseInt(br.readLine());
+        for (int test = 0; test < T; test++) {
+            int N = Integer.parseInt(br.readLine());
 
-            int[][] ladder = new int[100][100];
+            int[][] arr = new int[2][N+1];
+            int[][] dp = new int[2][N+1];
 
-            int startY = 99;
-            int startX = 0;
-            for (int i = 0; i < 100; i++) {
+            for (int i = 0; i < 2; i++) {
                 st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < 100; j++) {
-                    int value = Integer.parseInt(st.nextToken());
-                    ladder[i][j] = value;
-
-                    if (value == 2) {
-                        startX = j;
-                    }
+                for (int j = 1; j <= N; j++) {
+                    arr[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
 
-            Point start = new Point(startY, startX);
+            dp[0][1] = arr[0][1];
+            dp[1][1] = arr[1][1];
 
-            int[] dx = {-1,1};
-
-            while(true) {
-                if (start.y == 0) break;
-
-                boolean flag = false;
-                for (int i = 0; i < 2; i++) {
-                    int nx = start.x + dx[i];
-
-                    if (nx < 0 || nx >= 100) continue;
-                    if (ladder[start.y][nx] != 1) continue;
-                    if (flag) continue;
-
-                    start.setX(nx);
-                    flag = true;
-
-                    while(true) {
-                        nx = start.x + dx[i];
-
-                        if (nx < 0 || nx >= 100 || ladder[start.y][nx] != 1) break;
-
-                        start.setX(nx);
-                    }
-                }
-                start.setY(start.y - 1);
+            for (int i = 2; i <= N; i++) {
+                dp[0][i] = Math.max(dp[1][i-1], dp[1][i-2]) + arr[0][i];
+                dp[1][i] = Math.max(dp[0][i-1], dp[0][i-2]) + arr[1][i];
             }
 
-            System.out.printf("#%d %d\n", test, start.x);
+            System.out.println(Math.max(dp[0][N], dp[1][N]));
         }
+
+
     }
 }
