93a32e1258e48693e40712a35ba6331b0cd1c886
leedohyun <lucky77795@naver.com>
2023-11-28 16:06:53 +0900

    feat: 문제 추천 시스템 Version 1
    TreeSet 활용
    
    resolves: #21939

--- src/Main.java
+++ src/Main.java
@@ -9,42 +9,70 @@ public class Main {
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
 
+    static class Problem {
+        public int num;
+        public int diff;
 
+        public Problem(int num, int diff) {
+            this.num = num;
+            this.diff = diff;
+        }
+    }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int T = Integer.parseInt(br.readLine());
+        TreeSet<Problem> treeSet = new TreeSet<>((a,b) -> {
+            if (a.diff == b.diff) return a.num - b.num;
+            return a.diff - b.diff;
+        });
+        Map<Integer, Integer> map = new HashMap<>();
 
-        for (int test = 0; test < T; test++) {
-            TreeMap<Integer, Integer> map = new TreeMap<>();
-            int N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
-            for (int i = 0; i < N; i++) {
-                st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int P = Integer.parseInt(st.nextToken()); //문제 번호
+            int L = Integer.parseInt(st.nextToken()); //난이도
 
-                String order = st.nextToken();
-                int num = Integer.parseInt(st.nextToken());
+            treeSet.add(new Problem(P, L));
+            map.put(P, L);
+        }
 
-                switch (order) {
-                    case "I":
-                        map.put(num, map.getOrDefault(num, 0) + 1);
-                        break;
+        int M = Integer.parseInt(br.readLine());
 
-                    case "D":
-                        if (map.isEmpty()) continue;
-                        int key = (num == -1) ? map.firstKey() : map.lastKey();
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            String order = st.nextToken();
 
-                        if (map.get(key) > 0) map.put(key, map.get(key) - 1);
-                        if (map.get(key) == 0) map.remove(key);
-                        break;
-                }
-            }
-            if (map.isEmpty()) {
-                System.out.println("EMPTY");
-            } else {
-                System.out.println(map.lastKey() + " " + map.firstKey());
+            switch (order) {
+                case "add":
+                    int num = Integer.parseInt(st.nextToken());
+                    int diff = Integer.parseInt(st.nextToken());
+
+                    treeSet.add(new Problem(num, diff));
+                    map.put(num, diff);
+                    break;
+
+                case "recommend":
+                    int checker = Integer.parseInt(st.nextToken());
+
+                    if (checker == 1) {
+                        sb.append(treeSet.last().num).append("\n");
+                    } else {
+                        sb.append(treeSet.first().num).append("\n");
+                    }
+                    break;
+
+                case "solved":
+                    int number = Integer.parseInt(st.nextToken());
+
+                    treeSet.remove(new Problem(number, map.get(number)));
+                    map.remove(number);
+                    break;
             }
         }
+
+        System.out.print(sb);
     }
 }
