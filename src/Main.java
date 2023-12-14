a5a7ae87bebc5207f0ca93735ab9b4fd6cc26d05
leedohyun <lucky77795@naver.com>
2023-12-14 14:44:11 +0900

    feat: 2*n 타일링 2
    dp
    
    resolves: #11727

--- src/Main.java
+++ src/Main.java
@@ -16,10 +16,10 @@ public class Main {
 
         int[] dp = new int[N+1];
         dp[1] = 1;
-        dp[2] = 2;
+        dp[2] = 3;
 
         for (int i = 3; i <= N; i++) {
-            dp[i] = (dp[i-1] + dp[i-2]) % 10007;
+            dp[i] = (dp[i-1] + 2 * dp[i-2]) % 10007;
         }
 
         System.out.println(dp[N]);
